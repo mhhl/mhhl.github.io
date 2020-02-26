bfdeddd664bde2c7217b6d1f8135a9a16bad2350
mhhl <mario.94@live.com>
2020-02-26 00:34:21 -0600

    My first complete page

---
+++ cv.js
@@ -0,0 +1,13 @@
+var menuHover = document.getElementsByName("ul");
+var menuItem = document.getElementsByTagName("")
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
